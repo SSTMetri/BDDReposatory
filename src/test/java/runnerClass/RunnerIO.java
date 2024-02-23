91e85f7f191e20f60d9bc049f2b23581dc00f55d
soubhagya <soubhagyasm@gmail.com>
2024-02-23 20:39:06 +0530

    commit

--- src/test/java/runnerClass/RunnerIO.java
+++ src/test/java/runnerClass/RunnerIO.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 		features={".\\src\\test\\java\\featureFiles\\new.feature"},
 		glue={"stepDefinition"},
 //		tags ="@Smoke",
-		dryRun=false,  //true: when feature file is ewly created, browser will not launch but
+		dryRun=false,  //true: when feature file is newly created, browser will not launch but
 				     //all the scenarios will get annotation methods. 
 				     //It is going to match feature file and step definition file
 				     //false: execute
